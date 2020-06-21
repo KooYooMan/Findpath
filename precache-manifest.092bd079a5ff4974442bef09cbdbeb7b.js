625d766d0194dbe771609cfaaf8442514463764c
kooyooman <kooyooman@gmail.com>
2020-06-21 12:43:45 +0700

    Updates

---
+++ precache-manifest.092bd079a5ff4974442bef09cbdbeb7b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd5a745c9ee610683d9e497c43e2d6b",
+    "url": "/findpath/index.html"
+  },
+  {
+    "revision": "2b3f86089f28abb5d94b",
+    "url": "/findpath/static/css/main.ec0a761c.chunk.css"
+  },
+  {
+    "revision": "8e1e37ee9b7d885f8af3",
+    "url": "/findpath/static/js/2.c04d9cf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/findpath/static/js/2.c04d9cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3f86089f28abb5d94b",
+    "url": "/findpath/static/js/main.1ec499da.chunk.js"
+  },
+  {
+    "revision": "abf322ca4cc845ef34f3",
+    "url": "/findpath/static/js/runtime-main.746263c0.js"
+  },
+  {
+    "revision": "753af0caa2d5b6210ca8d2e95d4a414c",
+    "url": "/findpath/static/media/Correct.753af0ca.mp3"
+  },
+  {
+    "revision": "e59188c1502c0a7d15a43235fcf08132",
+    "url": "/findpath/static/media/GameOver.e59188c1.mp3"
+  },
+  {
+    "revision": "85b128e346c25211ab42696e32b5130e",
+    "url": "/findpath/static/media/Start.85b128e3.mp3"
+  },
+  {
+    "revision": "16ca1dfebb163d826099731556abd4c5",
+    "url": "/findpath/static/media/Wrong.16ca1dfe.mp3"
+  },
+  {
+    "revision": "2de71b50026221b2743ab5a0a8384f3a",
+    "url": "/findpath/static/media/carrot.2de71b50.jpg"
+  },
+  {
+    "revision": "189712385d2c268219936a24521dfdb7",
+    "url": "/findpath/static/media/eatingCarrot.18971238.mp3"
+  },
+  {
+    "revision": "6873511088d167c3ec7c1cbd1d426148",
+    "url": "/findpath/static/media/gameMusic.68735110.mp3"
+  },
+  {
+    "revision": "0e317adfd7be651185313b485fd3425b",
+    "url": "/findpath/static/media/tenor.0e317adf.gif"
+  }
+]);
